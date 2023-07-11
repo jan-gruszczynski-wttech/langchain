ee68e1b15b46613fcdfbbc10cd747fd241659dc2
jan-gruszczynski-wttech <110402781+jan-gruszczynski-wttech@users.noreply.github.com>
2023-07-11 13:36:48 +0200

    Update __init__.py

--- langchain/vectorstores/__init__.py
+++ langchain/vectorstores/__init__.py
@@ -28,8 +28,8 @@ from langchain.vectorstores.milvus import Milvus
 from langchain.vectorstores.mongodb_atlas import MongoDBAtlasVectorSearch
 from langchain.vectorstores.myscale import MyScale, MyScaleSettings
 from langchain.vectorstores.opensearch_vector_search import OpenSearchVectorSearch
-from langchain.vectorstores.pgembedding import PGEmbedding
-from langchain.vectorstores.pgvector import PGVector
+# from langchain.vectorstores.pgembedding import PGEmbedding
+# from langchain.vectorstores.pgvector import PGVector
 from langchain.vectorstores.pinecone import Pinecone
 from langchain.vectorstores.qdrant import Qdrant
 from langchain.vectorstores.redis import Redis
